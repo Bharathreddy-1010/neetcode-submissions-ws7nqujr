e8a7d679cd4a3dc5bdd2a9366c8e197035fb2d08
Bharathreddy-1010 <bharath404074@gmail.com>
2026-04-19 22:49:50 +0530

    Add: find-lucky-integer-in-an-array - submission-0

---
+++ Data Structures & Algorithms/find-lucky-integer-in-an-array/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        res = -1
+        for num in arr:
+            count = 0 
+            for a in arr:
+                if a == num:
+                    count+=1
+
+            if count == num:
+                res = max(res,num)
+
+        return res
